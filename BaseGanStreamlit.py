0546018fef91fa841bd583256e90168a68e40059
SwarJagdale <swarvjagdale@.com>
2024-08-20 23:27:10 +0530

    first commit

---
+++ BaseGanStreamlit.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import joblib
+import numpy as np
+from tensorflow import keras
+#predict new image using the generator and save it as newgen
+import matplotlib.pyplot as plt
+
+# Load the generator model
+gen = joblib.load('gan_images_mnist/GAN_gen.pkl')
+
+# Generate a new image
+generate=st.button('Generate New Image')
+if generate:
+    newgen = gen.predict(np.random.randn(1, 100))
+    newgen = newgen.reshape(28, 28)
+
+    # Display the new image using Streamlit
+    
+    plt.imshow(newgen, cmap='gray')
+    plt.axis('off')
+    st.pyplot(plt)
+    generate=False
